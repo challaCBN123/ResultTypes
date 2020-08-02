572f197b560f9e32a0eb7dd67850e2ddb6a769b5
challaCBN123 <challabhargava55@gmail.com>
2020-08-02 15:53:00 +0530

    successFullygetdata

--- swiftResultType/ViewController.swift
+++ swiftResultType/ViewController.swift
@@ -8,46 +8,64 @@
 
 import UIKit
 import Alamofire
-
+import SwiftyJSON
+struct dataUrl:Decodable{
+    var postId: Int
+    var id: Int
+   var name: String
+   var email: String
+    var body:String
+}
 enum PARAMETER_ENCODING{
     static let URL_ENCODING = "URLEncoding"
     static let JSON_ENCODING = "JSONEncoding"
 }
 class ViewController: UIViewController {
-let baseUrl = "http://jsonplaceholder.typicode.com/users"
+    //let newJob = DataUrl(jsonObjects: <#T##[String : Any]#>)
+//let baseUrl = "http://jsonplaceholder.typicode.com/users"
+    let baseUrl = "http://jsonplaceholder.typicode.com/comments"
     let endKey = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        gettingDataUsingUrl(baseUrl: baseUrl, serviceUrl: "", parameter: [:], encoding: PARAMETER_ENCODING.JSON_ENCODING) { (res) in
-            switch res{
-            case .success(let datasUrl):datasUrl.forEach({(DataUrl) in
-                print(DataUrl.name)
-            })
-                break
+        gettingDataUsingUrl(baseUrl: baseUrl, serviceUrl: endKey, encoding:PARAMETER_ENCODING.JSON_ENCODING) { (responseData) in
+            switch responseData{
                 
-            case .failure(let err):
-            print(err.localizedDescription)
+            case .success(let details):
+                details.forEach { (dataUrl) in
+                    print(dataUrl.email)
+                }
+                break
+            case .failure(_):
                 break
             }
         }
+       
     }
     
-   
- func gettingDataUsingUrl(baseUrl:String,serviceUrl:String,parameter:NSDictionary,encoding:String,CompletionHandler: @escaping(Result<[DataUrl],Error>)->Void){
-            if encoding == PARAMETER_ENCODING.JSON_ENCODING{
-                AF.request(baseUrl+serviceUrl,method: .get,parameters:(parameter as! Parameters),encoding:JSONEncoding.default).responseJSON { (responseData) in
-                    switch responseData.result{
-                    case .success(_):
-                        CompletionHandler(.success(responseData.value as! [DataUrl]))
-                        break
-                    case .failure(_):
-                        print("error")
-                        break
-                    }
-                    }
-                  
-                     
-            }
+    
+ func gettingDataUsingUrl(baseUrl:String,serviceUrl:String,encoding:String,CompletionHandler: @escaping(Result<[dataUrl],Error>)->Void){
+    AF.request(baseUrl,method: .get,encoding: JSONEncoding.default).responseJSON { (dataResponse) in
+      
+        switch dataResponse.result{
+            
+        case .success(_):
+            guard let data = dataResponse.data else { return }
+                do{
+                    let jsonData = try JSONDecoder().decode([dataUrl].self, from: data)
+                    CompletionHandler(.success(jsonData))
+                    //print(jsonData)
+                    
+                    //CompletionHandler(.success(jsonData))
+                    }catch{
+                               
+                     }
+            break
+        case .failure(_):
+           
+            break
+        }
+        
+    }
         }
         
     
--- swiftResultType/urlServices.swift
+++ swiftResultType/urlServices.swift
@@ -10,14 +10,24 @@ import Foundation
 import Alamofire
 import UIKit
 struct DataUrl:Decodable{
-    let id: Int
-    let name : String
-    let username : String
-    let email : String
-    let phone : String
-    let website: String
-    let address : [address]
-    let company : [company]
+    var id: Int
+    var name : String
+    var username : String
+    var email : String
+    var phone : String
+    var website: String
+    var address : [address]
+    var company : [company]
+    init(jsonObjects:[String:Any]) {
+        self.id = jsonObjects["id"] as? Int ?? 0
+        self.name = jsonObjects["name"] as? String ?? ""
+        self.username = jsonObjects["username"] as? String ?? ""
+        self.email = jsonObjects["email"] as? String ?? ""
+        self.phone = jsonObjects["phone"] as? String ?? ""
+        self.website = jsonObjects["website"] as? String ?? ""
+        self.address = jsonObjects["address"] as! [address]
+        self.company = jsonObjects["company"] as! [company]
+    }
 }
 struct address :Decodable{
     let street:String
@@ -36,3 +46,29 @@ struct company: Decodable{
     let bs : String
 }
 
+class datasURl : Decodable{
+    let id: Int?
+       let name : String?
+       let username : String?
+       let email : String?
+       let phone : String?
+       let website: String?
+    let address : [addresses]
+    let company : [comapnies]
+}
+class addresses : Decodable{
+    let street:String?
+       let suite:String?
+       let city:String?
+       let zipcode:String?
+    let geo : [geos]
+}
+class geos : Decodable{
+    let lat:String?
+       let lng:String?
+}
+class comapnies:Decodable{
+    let name : String?
+    let catchPhrase : String?
+    let bs : String?
+}
